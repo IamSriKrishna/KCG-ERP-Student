2294c99d95346fa7ca39721265360ba950bc19fb
bfkjabsdfkj <Hello@gmail.com>
2023-11-22 12:39:05 +0530

    library ui

--- lib/Feature/Screen/Library/Library.dart
+++ lib/Feature/Screen/Library/Library.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kcgerp/Provider/DarkThemeProvider.dart';
 import 'package:kcgerp/Util/util.dart';
@@ -11,14 +12,115 @@ class Library extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Provider.of<DarkThemeProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: theme.getDarkTheme?themeColor.darkTheme:themeColor.themeColor,
-        title: Text(
-          'Library.',
-          style: GoogleFonts.luxuriousRoman(
-            fontSize: 25
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: theme.getDarkTheme?themeColor.darkTheme:themeColor.themeColor,
+            title: Text(
+              'Library.',
+              style: GoogleFonts.luxuriousRoman(
+                fontSize: 25
+              ),
+            ),
           ),
-        ),
+          SliverList.builder(
+            itemCount: 3,
+            itemBuilder:(context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal:15.0,vertical: 5),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.22,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    color: theme.getDarkTheme?themeColor.darkTheme.withOpacity(0.15):themeColor.themeColor.withOpacity(0.2),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.22,
+                          width: MediaQuery.of(context).size.width * 0.32,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(15),
+                              bottomLeft: Radius.circular(15),
+                            ),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage('https://www.readm.org/uploads/chapter_files/cover/tbn/1587296988_198x0.jpg')
+                            )
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: MediaQuery.of(context).size.height * 0.05,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left:8.0),
+                                child: SizedBox(
+                                  height: MediaQuery.of(context).size.height * 0.045,
+                                  width: double.infinity,
+                                  child: Text(
+                                    'Engineering',
+                                    maxLines: 2,
+                                    style: GoogleFonts.luxuriousRoman(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left:8.0,top: 8),
+                                child: Text(
+                                  '16 Authors',
+                                  style: GoogleFonts.luxuriousRoman(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w300
+                                  ),
+                                ),
+                              ),
+                              Gap(20),
+                              Container(
+                                width: double.infinity,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        '${24}',
+                                        style: GoogleFonts.luxuriousRoman(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold
+                                        ),
+                                      ),
+                                      Text(
+                                        ' Books',
+                                        style: GoogleFonts.luxuriousRoman(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w300
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        )
+                      ],
+                    ),
+                  ),
+                  ),
+              );
+          },)
+        ],
       ),
     );
   }
--- lib/Model/LibraryData.dart
+++ lib/Model/LibraryData.dart
@@ -1,14 +1,50 @@
-// class Library{
-//   final String id;
-//   final String title;
-//   final String bookno;
-//   final int available;
-//   final String fcmtoken;
-//   final String department;
-//   final String token;
-//   final String dp;
-//   final String Studentclass;
-//   final String year;
-//   final List<dynamic> followers;
-//   final List<dynamic> following;
-// }
+import 'dart:convert';
+
+class LibraryModel {
+  final String id;
+  final String book_title;
+  final List<dynamic> access_no;
+  final List<String> authors;
+  LibraryModel({
+    required this.id,
+    required this.book_title,
+    required this.access_no,
+    required this.authors,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'book_title': book_title,
+      'access_no':access_no,
+      'authors': authors,
+    };
+  }
+
+  factory LibraryModel.fromMap(Map<String, dynamic> map) {
+    return LibraryModel(
+      id: map['_id'] ?? '',
+      book_title: map['book_title'] ?? '',
+      access_no:map['access_no']??[],
+      authors:map['authors']??[],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory LibraryModel.fromJson(String source) => LibraryModel.fromMap(json.decode(source));
+
+  LibraryModel copyWith({
+    String? id,
+    String? book_title,
+    List<dynamic>? access_no,
+    List<String>? authors,
+  }) {
+    return LibraryModel(
+      id: id ?? this.id,
+      book_title: book_title ?? this.book_title,
+      access_no: access_no??this.access_no,
+      authors: authors??this.authors
+    );
+  }
+}
